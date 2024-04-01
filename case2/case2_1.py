56be23f3be56a0101466db8e8b54a7e54c083970
Sabrina <sabvan4134@gmail.com>
2024-04-01 13:02:42 -0400

    add case 2 folder

---
+++ case2/case2_1.py
@@ -0,0 +1,33 @@
+'''
+Rl method
+ - states are different weights 
+ - rewards are based on return, risk, and Sharpe ratio
+ - 
+
+
+Markov Chains?
+ - model transitions between returns
+ - each state represents a particular market 
+ - learn probabilities
+ - if predict a transition to a different market regime, can rebalance portfolios 
+ 
+Blacklitterman
+ - 
+ 
+Markowitz:
+ - minimize wC^tw
+ - calculate efficient frontier weights using quadratic programming
+
+Sharpe Ratio Maximization
+ - use monte carlo simulation to find to best weights
+ - might take too long?
+
+projectsion onto Laplace distribution
+ - rather than on normal, you laplace that captures heavty tailed asset returns
+ - estimate parameters from there than optimize further
+
+
+run model on new data!!
+
+
+'''
