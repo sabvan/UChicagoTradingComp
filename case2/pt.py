d3ebc9773cbb23e324e4e45b4a4493a746c6680d
sabvan <67799438+sabvan@users.noreply.github.com>
2024-04-15 19:23:13 -0400

    final submitted code

--- case2/pt.py
+++ case2/pt.py
@@ -6,10 +6,7 @@ from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import os
-import tensorflow as tf
-from tensorflow.keras.layers import LSTM, Flatten, Dense
-from tensorflow.keras.models import Sequential
-import tensorflow.keras.backend as K
+
 
 print(pd.__version__) 
 
@@ -17,116 +14,6 @@ data = pd.read_csv("testdata7.csv", index_col = 0)
 
 TRAIN, TEST = train_test_split(data, test_size = 0.2, shuffle = False)
 
-# TO GRADERS: If the code does not run or 
-# takes too long, use the commented out
-# Allocator
-
-class Allocator():
-    def __init__(self, train_data):
-        '''
-        Anything data you want to store between days must be stored in a class field
-        '''
-        self.running_price_paths = train_data.copy()
-        self.train_data = train_data.copy()
-        self.running_weights_paths = [[1/6, 1/6, 1/6, 1/6, 1/6, 1/6]]
-        
-        # Do any preprocessing here -- do not touch running_price_paths, it will store the price path up to that data
-        
-    
-    class Model:
-        def __init__(self):
-            self.data = None
-            self.model = None
-            
-        def __build_model(self, input_shape, outputs):
-            '''
-            Builds and returns the Deep Neural Network that will compute the allocation ratios
-            that optimize the Sharpe Ratio of the portfolio
-            
-            inputs: input_shape - tuple of the input shape, outputs - the number of assets
-            returns: a Deep Neural Network model
-            '''
-            model = Sequential([
-                LSTM(64, input_shape=input_shape),
-                Flatten(),
-                Dense(outputs, activation='softmax')
-            ])
-            
-            
-
-            def sharpe_loss(_, y_pred):
-                # make all time-series start at 1
-                data = tf.divide(self.data, self.data[0])  
-                
-                # value of the portfolio after allocations applied
-                portfolio_values = tf.reduce_sum(tf.multiply(data, y_pred), axis=1) 
-                portfolio_returns = (portfolio_values[1:] - portfolio_values[:-1]) / portfolio_values[:-1]  # % change formula
-                sharpe = K.mean(portfolio_returns) / K.std(portfolio_returns)
-                
-                # since we want to maximize Sharpe, while gradient descent minimizes the loss, 
-                #   we can negate Sharpe (the min of a negated function is its max)
-                return -sharpe
-            
-            model.compile(loss=sharpe_loss, optimizer='adam')
-            return model
-        
-        def get_allocations(self, data):
-            data = pd.DataFrame(data, columns=['0', '1', '2', '3', '4', '5'])
-            '''
-            Computes and returns the allocation ratios that optimize the Sharpe over the given data
-            input: data - DataFrame of historical closing prices of various assets
-            return: the allocations ratios for each of the given assets
-            '''
-            
-            # data with returns
-            data_w_ret = np.concatenate([data.values[1:], data.pct_change().values[1:] ], axis=1)
-
-            data = data.iloc[1:]
-            self.data = tf.cast(tf.constant(data), float)
-            
-            if self.model is None:
-                self.model = self.__build_model(data_w_ret.shape, len(data.columns))
-
-            fit_predict_data = data_w_ret[np.newaxis,:]        
-            self.model.fit(fit_predict_data, np.zeros((1, len(data.columns))), epochs=10, shuffle=False)
-            return self.model.predict(fit_predict_data)[0]
-        
-    def calculateWeights(self, pricePath, running_weights_paths, historydays):
-        mod = self.Model()
-        return mod.get_allocations(pricePath)
-    
-    
-    def allocate_portfolio(self, asset_prices):
-        '''
-        asset_prices: np array of length 6, prices of the 6 assets on a particular day
-        weights: np array of length 6, portfolio allocation for the next day
-        '''
-        ### edit: used np.append to make this work
-        self.running_price_paths = np.append(self.running_price_paths, asset_prices)
-    
-        ### TODO Implement your code here
-        #print(self.running_price_paths)
-        
-        historydays = 100
-        
-        paths1 = self.running_price_paths[-historydays * 6:].T
-        pricePaths = []
-        for i in range(historydays):
-            sub = paths1[i*6: i*6+6]
-            pricePaths.append(sub)
-        pricesPaths = pd.DataFrame(pricePaths, columns=['0', '1', '2', '3', '4', '5'])
-        
-            
-        weights = self.calculateWeights(pricePaths, self.running_weights_paths, historydays)
-        self.running_weights_paths.append(weights)
-        
-        return weights
-
-
-
-
-
-'''
 class Allocator():
     def __init__(self, train_data):
         
@@ -228,8 +115,6 @@ class Allocator():
         return weights
 
 
-'''
-
 def grading(train_data, test_data): 
     '''
     Grading Script
@@ -274,7 +159,5 @@ plt.show()
 
 print(sharpe)
 
-
-
-# creds https://github.com/shilewenuw/deep-learning-portfolio-optimization/blob/main/Model.py
+# creds
 # https://github.com/PaiViji/PythonFinance-PortfolioOptimization/blob/master/Lesson6_SharpeRatioOptimization/Lesson6_MainContent.ipynb
